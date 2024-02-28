85b4bea49d0d71307a7343989931e9b3137f8956
unknown <jumpman1483@hotmail.com>
2024-02-28 05:16:18 -0600

    added code and kept debugging to get a started/finished product

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -26,7 +26,7 @@ export default function Resume() {
         <div className="resume-content">
           <div className="resume-section">
             <h3>Download Resume</h3>
-            {/* reference: https://www.codeease.net/programming/javascript/Make-a-file-downloadable-in-React */}
+            {/**/}
             <p>Click the link below to download my resume:</p>
             <a href="" download>Download Resume</a>
           </div>
